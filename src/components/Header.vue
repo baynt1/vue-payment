36f8f513623755789f780be44b0f3109b4f4c31f
algafur <algafura@gmail.com>
2024-05-24 16:12:46 +0500

    feat: maket

---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>Vue payment</h2>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,37 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>{{ transaction.amount }}</span>
+      <button class="delete-btn">x</button>
+    </li>
+    <!--    <li class="minus">-->
+    <!--      Cash <span>-$400</span><button class="delete-btn">x</button>-->
+    <!--    </li>-->
+
+    <!--    <li class="plus">-->
+    <!--      Paycheck <span>$800</span><button class="delete-btn">x</button>-->
+    <!--    </li>-->
+  </ul>
+</template>
+
+<script>
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: "1", amount: 1 },
+      { id: 2, text: "2", amount: 2 },
+      { id: 3, text: "3", amount: -3 },
+      { id: 4, text: "4", amount: -4 },
+    ];
+
+    return {
+      transactions,
+    };
+  },
+};
+</script>
